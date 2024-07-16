b4fbfc0cbfdec37b69f3051b99cde78ae606f995
Eddy Juveno BOCQUELIN <bocquelin@gmail.com>
2024-07-16 12:23:15 +0300

    modif ttls main

--- main.go
+++ main.go
@@ -8,78 +8,12 @@ import (
 )
 
 var (
-	customTransport = http.DefaultTransport
-)
-
-func andleRequest(w http.ResponseWriter, r *http.Request) {
-	targetURL := r.URL
-
-	proxyReq, err := http.NewRequest(r.Method, targetURL.String(), r.Body)
-	if err != nil {
-		http.Error(w, "Error creating proxy request", http.StatusInternalServerError)
-		log.Printf("Error creating proxy request: %v", err)
-		return
-	}
-
-	for name, values := range r.Header {
-		for _, value := range values {
-			proxyReq.Header.Set(name, value)
-		}
-	}
-
-	resp, err := customTransport.RoundTrip(proxyReq)
-	if err != nil {
-		http.Error(w, "Error forwarding request", http.StatusInternalServerError)
-		log.Printf("Error forwarding request: %v", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	for name, values := range resp.Header {
-		for _, value := range values {
-			w.Header().Set(name, value)
-		}
-	}
-
-	w.WriteHeader(resp.StatusCode)
-
-	if _, err := io.Copy(w, resp.Body); err != nil {
-		log.Printf("Error copying response body: %v", err)
-	}
-}
-
-// func main() {
-// 	remote, err := url.Parse("https://www.youtube.com/")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	handler := func(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
-// 		return func(w http.ResponseWriter, r *http.Request) {
-// 			log.Println(r.URL)
-// 			r.Host = remote.Host
-// 			w.Header().Set("X-Ben", "Rad")
-// 			p.ServeHTTP(w, r)
-// 		}
-// 	}
-
-// 	proxy := httputil.NewSingleHostReverseProxy(remote)
-// 	proxy.Transport = &http.Transport{
-// 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-// 	}
-// 	http.HandleFunc("/", handler(proxy))
-// 	err = http.ListenAndServe(":8080", nil)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// }
-
-var (
 	// URL de base du serveur de destination
 	baseURL *url.URL
 )
 
 func init() {
-	// Spécifiez l'URL de base ici
+	// Spécifiez l'URL de base ici avec le schéma
 	var err error
 	baseURL, err = url.Parse("https://www.youtube.com") // Remplacez par votre URL de base
 	if err != nil {
