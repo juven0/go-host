b584c91f55c237e499498317225cb2ac92ff0baa
Eddy Juveno BOCQUELIN <bocquelin@gmail.com>
2024-07-30 08:58:24 +0300

    amelioration du partie server et modification des config

--- internal/configs/config.go
+++ internal/configs/config.go
@@ -2,6 +2,8 @@ package configs
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/spf13/viper"
@@ -23,14 +25,26 @@ type Configuration struct {
 
 func NewConfiguration() (*Configuration, error) {
 	var Config *Configuration
-	viper.AddConfigPath("data")
+	workDir, err := os.Getwd()
+	if err != nil {
+		return nil, fmt.Errorf("error getting working directory: %s", err)
+	}
+
+	// Remonter d'un niveau si nous sommes dans le dossier 'cmd'
+	if filepath.Base(workDir) == "cmd" {
+		workDir = filepath.Dir(workDir)
+	}
+
+	// Définir le chemin du dossier 'data'
+	configPath := filepath.Join(workDir, "data")
+	viper.AddConfigPath(configPath)
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
 
 	viper.AutomaticEnv()
 	viper.SetEnvKeyReplacer(strings.NewReplacer(`.`, `_`))
 
-	err := viper.ReadInConfig()
+	err = viper.ReadInConfig()
 	if err != nil {
 		return nil, fmt.Errorf("error loading config file: %s", err)
 	}
