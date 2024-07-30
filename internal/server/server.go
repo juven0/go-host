30b6ac5b2524093a510baea729abc5f1f73cf3dd
Eddy Juveno BOCQUELIN <bocquelin@gmail.com>
2024-07-30 08:34:15 +0300

    amelioration du partie server

--- internal/server/server.go
+++ internal/server/server.go
@@ -16,10 +16,18 @@ func Run() error {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/ping", ping)
 	for _, resource := range config.Resources {
-		url, _ := url.Parse(resource.Destination_URL)
+		url, err := url.Parse(resource.Destination_URL)
+		if err != nil {
+			return fmt.Errorf("invalid destination URL for endpoint %s: %v", resource.Endpoint, err)
+		}
 		proxy := NewProxy(url)
+		if proxy == nil {
+			return fmt.Errorf("failed to create proxy for endpoint %s", resource.Endpoint)
+		}
 		mux.HandleFunc(resource.Endpoint, ProxyRequestHandler(proxy, url, resource.Endpoint))
 	}
+	serverAddr := fmt.Sprintf("%s:%s", config.Server.Host, config.Server.Listen_port)
+	fmt.Printf("Starting server on %s\n", serverAddr)
 	if err := http.ListenAndServe(config.Server.Host+":"+config.Server.Listen_port, mux); err != nil {
 		return fmt.Errorf("could not start the server: %v", err)
 	}
